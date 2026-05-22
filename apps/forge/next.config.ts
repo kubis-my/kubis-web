030c9d5e222af6df1987bfdfc79fb2e8ee4eff9e
kashi93 <75873210+kashi93@users.noreply.github.com>
2026-05-22 22:02:09 +0800

    feat: add Forge service GraphQL URL to headers configuration

--- apps/forge/next.config.ts
+++ apps/forge/next.config.ts
@@ -8,7 +8,10 @@ const nextConfig: NextConfig = {
         root: path.join(__dirname, '../../'),
     },
     async headers() {
-        return getDefaultHeaders([process.env.NEXT_PUBLIC_ACCOUNT_SERVICE_GRAPHQL_URL!]);
+        return getDefaultHeaders([
+            process.env.NEXT_PUBLIC_ACCOUNT_SERVICE_GRAPHQL_URL!,
+            process.env.NEXT_PUBLIC_FORGE_SERVICE_GRAPHQL_URL!,
+        ]);
     },
 };
 
